481adceece3507dc4346e1a4d1d70fb566b428a4
Vicente Gutiérrez <gtz.rvera@gmail.com>
2024-04-18 21:01:23 -0600

    initial commit

---
+++ shortener_app/config.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    env_name: str = "Local"
+    base_url: str = "http://127.0.0.1:8000"
+    db_url: str = "sqlite:///./shortener.db"
+
+    class Config:
+        env_file: str = ".env"
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for {settings.env_name}")
+
+    return settings
