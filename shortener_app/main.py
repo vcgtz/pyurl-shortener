6876008cba23d24aab8b8ae76cfb26efbb15bf10
Vicente Gutiérrez <gtz.rvera@gmail.com>
2024-04-20 12:24:00 -0600

    add redirection when accesing to a shortened url

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,15 +1,16 @@
 import secrets
 import validators
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 from .database import SessionLocal, engine
 
-
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -19,8 +20,15 @@ def get_db():
     finally:
         db.close()
 
+
 def raise_bad_request(message: str):
     raise HTTPException(status_code=400, detail=message)
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+
+    raise HTTPException(status_code=404, detail=message)
 
 
 @app.get("/")
@@ -52,3 +60,17 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
