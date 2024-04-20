5ed9b3c731d643a6a1a70b997384b97ae4e051da
Vicente Gutiérrez <gtz.rvera@gmail.com>
2024-04-20 13:36:22 -0600

    return the complete url when creating a new short url

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -3,9 +3,12 @@ import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+from .config import get_settings
+
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -18,6 +21,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+
+    return db_url
 
 
 def raise_bad_request(message: str):
@@ -42,10 +58,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
 
     db_url = crud.create_db_url(db=db, url=url)
 
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-
-    return db_url
+    return get_admin_info(db_url)
 
 
 @app.get("/{url_key}")
@@ -58,5 +71,13 @@ def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(
 
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.get("/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo)
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_secret_key(db=db, secret_key=secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
